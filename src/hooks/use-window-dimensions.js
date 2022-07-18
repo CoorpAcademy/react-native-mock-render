8740826ccc14474faa9321573ac56d61fd1fe003
chrisdugne <chris.dugne@uralys.com>
2022-07-18 18:46:18 +0200

    0.1.14

--- src/hooks/use-window-dimensions.js
+++ src/hooks/use-window-dimensions.js
@@ -2,9 +2,7 @@
  * https://github.com/facebook/react-native/blob/main/Libraries/Utilities/useWindowDimensions.js
  */
 const useWindowDimensions = () => {
-  const dimensions = {};
-  const setDimensions = () => {};
-  return [dimensions, setDimensions];
+  return {width: 1000, height: 1000};
 };
 
 module.exports = useWindowDimensions;
